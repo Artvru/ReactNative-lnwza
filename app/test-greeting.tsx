d74bad091b966a65ea5b4c853e420a7c5b9efe98
Artvru <151433841+Artvru@users.noreply.github.com>
2025-12-05 20:00:38 +0700

    Add files via upload

---
+++ app/test-greeting.tsx
@@ -0,0 +1,13 @@
+import Greeting from "@/components/greeting";
+import { View } from "react-native";
+{
+     (
+
+
+  <View style={{alignItems: 'center', top: 50}}>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
+      </View>
+    );
+}

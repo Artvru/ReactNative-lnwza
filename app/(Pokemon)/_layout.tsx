8438e1ec2fcda6371b8963d4c678d3ed6267211b
Teerarak66222420013 <151433841+Artvru@users.noreply.github.com>
2026-02-12 19:25:45 +0700

    Add files via upload

---
+++ app/(Pokemon)/_layout.tsx
@@ -0,0 +1,33 @@
+import { FontAwesome } from '@expo/vector-icons';
+import { Tabs } from 'expo-router';
+
+export default function PokemonLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'orange' }}>
+      <Tabs.Screen
+        name="charmander"
+        options={{
+          title: 'Charmander',
+          tabBarIcon: ({ color }) => <FontAwesome name="fire" size={24} color={color} />,
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="pikachu"
+        options={{
+          title: 'Pikachu',
+          tabBarIcon: ({ color }) => <FontAwesome name="bolt" size={24} color={color} />,
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="ivysour"
+        options={{
+          title: 'Ivysour',
+          tabBarIcon: ({ color }) => <FontAwesome name="leaf" size={24} color={color} />,
+          headerShown: false,
+        }}
+      />
+    </Tabs>
+  );
+}
